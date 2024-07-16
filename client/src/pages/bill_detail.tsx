f5a6b73620525d1143b0ef48acef755f45c6259f
vaibhav494 <vaibhavgaming190@gmail.com>
2024-07-16 20:39:25 +0530

    helloworld

--- client/src/pages/bill_detail.tsx
+++ client/src/pages/bill_detail.tsx
@@ -1,10 +1,16 @@
-function Bill_detail(){
-    return(
-        <>
-        <table>
-            <tr>hello</tr>
-        </table>
-        </>
-    )
+function Bill_detail() {
+  return (
+    <>
+      <table className="bill_detail_main">
+        <tr>
+          <th>Seller Name</th>
+          <th>Buyer Name</th>
+          <th>Invoice Number</th>
+          <th>Invoice Date</th>
+          <th>Total Amount</th>
+        </tr>
+      </table>
+    </>
+  );
 }
 export default Bill_detail;
