00e170127d8e565ce977530ebe71d67db6dcca19
vaibhav494 <vaibhavgaming190@gmail.com>
2024-07-17 11:19:06 +0530

    bill_detail page updated

--- client/src/pages/bill_detail.tsx
+++ client/src/pages/bill_detail.tsx
@@ -1,35 +1,51 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-const [detail, setDetail] = useState<any>([])
+
+interface InvoiceDetail {
+  Seller_Name: string;
+  Buyer_Name: string;
+  Invoice_Number: string;
+  Invoice_Date: string;
+  Total_Amount: number;
+}
 function Bill_detail() {
+    const [detail, setDetail] = useState<InvoiceDetail[]>([]);
+
     useEffect(() => {
         axios
             .get("http://localhost:4000/insert_full_invoice_detail")
-            .then((invoice_detail) => setDetail(invoice_detail))
+            .then((response) => setDetail(response.data))
             .catch((err) => {
-            console.log(err);
+                console.log(err);
             });
-    }, []); 
-    function generate(){
-        <tr>
-            <th></th>
-        </tr>
-    }
-    
-  return (
-    <>
-      <table className="bill_detail_main">
-        <tr>
-          <th>Seller Name</th>
-          <th>Buyer Name</th>
-          <th>Invoice Number</th>
-          <th>Invoice Date</th>
-          <th>Total Amount</th>
-          
-        </tr>
-        
-      </table>
-    </>
-  );
+    }, []);
+
+    return (
+        <>
+            <table className="bill_detail_main">
+                <thead>
+                    <tr>
+                        <th>Seller Name</th>
+                        <th>Buyer Name</th>
+                        <th>Invoice Number</th>
+                        <th>Invoice Date</th>
+                        <th>Total Amount</th>
+                    </tr>
+                </thead>
+                <tbody className="bill_detail_content">
+                    {detail.map((invoice, index) => (
+                        <tr key={index}>
+                            <td>{invoice.Seller_Name}</td>
+                            <td>{invoice.Buyer_Name}</td>
+                            <td>{invoice.Invoice_Number}</td>
+                            <td>{invoice.Invoice_Date}</td>
+                            <td>{invoice.Total_Amount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
 }
+
 export default Bill_detail;
